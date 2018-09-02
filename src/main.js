319a16c019325f376e049a99cba8390916cb2d9f
MaliciousHex <harryermawan@gmail.com>
2018-09-02 21:26:32 +0700

    setup firebase

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+/* eslint semi: ["error", "never"] */
 import Vue from 'vue'
-import App from './App'
 import router from './router'
+import App from './components/App'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  render: h => h(App),
   router,
-  components: { App },
-  template: '<App/>'
+  // components: { App },
+  // template: '<App/>',
 })
